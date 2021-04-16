68a8eaf3def0da6c7e1f17c8d6d2fc2097df9832
VSLG <vlopez_jv@hotmail.com>
2021-04-15 22:20:33 -0700

    added api routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,2 +1,9 @@
 //this file will contain al of the user-facing routes, ex:
 //homepage and login
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  res.render('homepage');
+});
+
+module.exports = router;
